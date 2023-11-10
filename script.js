1946f68a1365e4d7e85da9adbc09d56f8482dc29
pancsaa <pampukp@gmail.com>
2023-11-10 14:19:47 +0100

    huzocska

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const button=document.querySelector("button");
+const inputs=document.querySelectorAll("input");
 
-button.onclick=(event)=>{
+const red=document.querySelector("input:first-child");
+const green=document.querySelector("input:nth-child(2)");
+const blue=document.querySelector("input:last-of-type");
+const circle=document.querySelector(".circle");
+
+inputs.forEach(inp => {
+    inp.addEventListener("input", () =>{
+        circle.style.backgroundColor=`rgb(${red.value}, ${green.value}, ${blue.value})`;
+    })
+})
+
+/*
+input.onclick=(event)=>{
     const red=document.querySelector("input:first-child");
     const green=document.querySelector("input:nth-child(2)");
     const blue=document.querySelector("input:last-of-type");
     const circle=document.querySelector(".circle");
 
     circle.style.backgroundColor=`rgb(${red.value}, ${green.value}, ${blue.value})`;
-}
+}*/
